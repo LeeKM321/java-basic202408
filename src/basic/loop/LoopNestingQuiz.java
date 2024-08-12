a5cf1689bced3b55ac396ea9d911088af288b6cd
LeeKM321 <stephen4951@gmail.com>
2024-08-12 18:25:18 +0900

    Day02 - 반복문 관련 문제

---
+++ src/basic/loop/LoopNestingQuiz.java
@@ -0,0 +1,17 @@
+package basic.loop;
+
+public class LoopNestingQuiz {
+    public static void main(String[] args) {
+
+        /*
+		 - 정수를 하나 입력받아서 해당 숫자까지의 모든 소수를
+		  가로로 출력하고, 그 소수들의 개수를 구하는 로직을 작성합니다.
+
+		 ex)
+		 입력받은 수 -> 30
+		 소수: 2 3 5 7 11 13 17 19 23 29
+		 소수의 개수: 10개
+		*/
+
+    }
+}
---
+++ src/basic/loop/WhileQuiz02.java
@@ -0,0 +1,14 @@
+package basic.loop;
+
+public class WhileQuiz02 {
+
+    public static void main(String[] args) {
+
+        /*
+		 - 정수 1개를 입력받아서 해당 정수가 소수(prime number)
+		  인지를 판별하시면 됩니다.
+		  소수: 약수가 1과 자기 자신만 있는 수.
+		 */
+
+    }
+}
