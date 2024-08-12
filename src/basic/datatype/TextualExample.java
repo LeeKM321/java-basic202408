3ca22f290705131bcc5ddbdf1da3a65c0270c070
LeeKM321 <stephen4951@gmail.com>
2024-08-12 09:27:12 +0900

    Day02 - TextualExample 내용 추가

--- src/basic/datatype/TextualExample.java
+++ src/basic/datatype/TextualExample.java
@@ -19,6 +19,18 @@ public class TextualExample {
 
         String str = "hello";
 
+        String s1 = "my dream ";
+        String s2 = "is a programmer!";
+
+        // 문자열의 덧셈 연산 -> 문자열을 단순히 연결해서 결합한 결과.
+        // 덧셈만 가능! 뺄셈, 곱셈, 나눗셈은 허용하지 않음!
+        System.out.println(s1 + s2);
+
+        // 문자열과 다른 데이터 타입 간의 덧셈 연산 -> 결과값이 무조건 문자열 덧셈 연산과 같다.
+        int month = 10;
+        int day = 13;
+
+        System.out.println("내 생일은 " + month + "월 " + day + "일 입니다.");
 
     }
 }
