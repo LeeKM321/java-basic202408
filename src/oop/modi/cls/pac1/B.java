85255354cbe91f6f9c7639ef1933b828441f389c
LeeKM321 <stephen4951@gmail.com>
2024-08-14 18:05:21 +0900

    Day04 - 접근제한자(클래스)

---
+++ src/oop/modi/cls/pac1/B.java
@@ -0,0 +1,10 @@
+package oop.modi.cls.pac1;
+
+// public 제한자는 접근 제한이 없는 형태 (어디에서나 접근 가능)
+public class B {
+
+    // 클래스 A의 접근제한자는 default
+    // 동일 패키지 내부의 클래스에는 접근을 허용.
+    A a = new A();
+
+}
---
+++ src/oop/modi/cls/pac2/C.java
@@ -0,0 +1,10 @@
+package oop.modi.cls.pac2;
+
+import oop.modi.cls.pac1.B;
+
+public class C {
+
+//    A a = new A(); (x) default -> 패키지가 달라지만 접근 x
+    B b = new B(); // public
+
+}
